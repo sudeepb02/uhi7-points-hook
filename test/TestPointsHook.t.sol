f40c419c0f502d47659bb7b81eed81f0fba836dd
Sudeep Biswas <sudeepb02@gmail.com>
2025-11-04 22:16:32 +0530

    feat: add initial points hook

---
+++ test/TestPointsHook.t.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Test} from "forge-std/Test.sol";
+
+import {Deployers} from "@uniswap/v4-core/test/utils/Deployers.sol";
+import {PoolSwapTest} from "v4-core/test/PoolSwapTest.sol";
+import {MockERC20} from "solmate/src/test/utils/mocks/MockERC20.sol";
+
+import {PoolManager} from "v4-core/PoolManager.sol";
+import {SwapParams, ModifyLiquidityParams} from "v4-core/types/PoolOperation.sol";
+import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
+
+import {Currency, CurrencyLibrary} from "v4-core/types/Currency.sol";
+import {PoolId} from "v4-core/types/PoolId.sol";
+
+import {Hooks} from "v4-core/libraries/Hooks.sol";
+import {TickMath} from "v4-core/libraries/TickMath.sol";
+import {SqrtPriceMath} from "v4-core/libraries/SqrtPriceMath.sol";
+import {LiquidityAmounts} from "@uniswap/v4-core/test/utils/LiquidityAmounts.sol";
+
+import {ERC1155TokenReceiver} from "solmate/src/tokens/ERC1155.sol";
+
+import "forge-std/console.sol";
+import {PointsHook} from "../src/PointsHook.sol";
+
+contract TestPointsHook is Test, Deployers, ERC1155TokenReceiver {
+    MockERC20 token; // our token to use in the ETH-TOKEN pool
+
+    // Native tokens are represented by address(0)
+    Currency ethCurrency = Currency.wrap(address(0));
+    Currency tokenCurrency;
+
+    PointsHook hook;
+
+    function setUp() public {
+        // TODO
+    }
+}
